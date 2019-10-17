8cb5a3ab2ea447ea39725926884e81a827347ca0
keyso <keyso000@outlook.com>
2019-10-17 11:50:42 +0800

    update a demo project

---
+++ Assets/LoadAssetsBundles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadAssetsBundles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var myLoadedAssetBundle
+          = AssetBundle.LoadFromFile(Path.Combine("Assets/AssetBundles", "lightmapmodel"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+
+        GameObject.Instantiate(myLoadedAssetBundle.LoadAsset("CubeAndSphere", typeof(GameObject)));
+        GameObject.Instantiate(myLoadedAssetBundle.LoadAsset("Tower", typeof(GameObject)));
+        GameObject.Instantiate(myLoadedAssetBundle.LoadAsset("Cube", typeof(GameObject)));
+    }
+	
+}
